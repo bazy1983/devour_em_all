75a8bdacca34633e506a446ebe0bd2af9b4c12c1
Mohammed Hameed <mohammed.hameed@email.com>
2018-05-06 17:04:14 -0500

    added logs for sql

--- config/orm.js
+++ config/orm.js
@@ -5,6 +5,7 @@ var orm = {
     selectAllBurger: function (table, callback) { //return all burger types to home page
         let query = "SELECT * FROM ??"
         connection.query(query, table, function (err, sqlResults) {
+            console.log("SQL selectAllBurger line 8")
             if (err) throw err;
             callback(sqlResults); //callback function to send data to controller js
         })
@@ -13,6 +14,7 @@ var orm = {
     selectOneBurger: function (table, id, callback){
         let query = "SELECT * FROM ?? WHERE id = ?";
         connection.query(query, [table, id], function(err, sqlResult){
+            console.log("selectOneBurger line 17")
             if (err) throw err;
             callback(sqlResult);
         })
@@ -21,7 +23,9 @@ var orm = {
     OdrderBurger : function(table, clientData, callback){
         
         let sqlQuery = "INSERT INTO ?? SET ?"
+        //clientData is object that hold burger info
         connection.query(sqlQuery, [table, clientData], function(err, sqlResult){
+            console.log("OrderBurger line 28")
             if (err) throw err;
             callback(sqlResult);
         })
@@ -31,6 +35,7 @@ var orm = {
         
         let sqlQuery = "SELECT * FROM ?? WHERE DATE(created_at) = curdate()"
         connection.query(sqlQuery, table, function(err, sqlResult){
+            console.log("AllOrders line 38")
             if (err) throw err;
             callback(sqlResult);
         })
@@ -39,6 +44,7 @@ var orm = {
     devour : function(table, clientData, callback){
         let sqlQuery = "UPDATE ?? SET ? WHERE ?"
         connection.query(sqlQuery, [table, {devoured : true}, clientData], function(err, sqlResult){
+            console.log("devour line 47")
             if (err) throw err;
             callback(sqlResult)
         })
@@ -47,6 +53,7 @@ var orm = {
     makeNewBurger : function(table, clientData, callback){
         let sqlQuery = "INSERT INTO ?? SET ?"
         connection.query(sqlQuery, [table, clientData], function(err, sqlResult){
+            console.log("makeNewBurger line 56")
             if (err) throw err;
             callback()
         })
